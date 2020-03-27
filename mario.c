9dd77f1bacc86ebd1f2eaac7c1e9aa39cb7d363b
aadhiraj <aadhiraj@users.noreply.github.com>
2020-03-27 13:22:35 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -1,28 +1,27 @@
 #include <cs50.h>
 #include <stdio.h>
-void repeat (string c, int n);
+void repeat(string c, int n);
 
 int main(void)
 {
-   int number;
-   do
+   int number; do
    {
-       number = get_int ("Height: ");
+       number = get_int("Height: ");
    }
-   while (number < 1 || number > 8);
+   while(number < 1 || number > 8);
 
-   for (int i = 0; i < number; i++)
+   for(int i = 0; i < number; i++)
    {
-       repeat (" ", (number - i - 1));
-       repeat ("#", (i+1));
-       printf ("\n");
+       repeat(" ", (number - i - 1));
+       repeat("#", (i+1));
+       printf("\n");
    }
 }
 
-void repeat (string c, int n)
+void repeat(string c, int n)
 {
-    for (int i = 0; i < n; i++)
+    for(int i = 0; i < n; i++)
     {
-        printf ("%s", c);
+        printf("%s", c);
     }
 }
