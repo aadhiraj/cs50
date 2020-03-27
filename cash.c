9ab27c6f4d38f8293287c6963739f6f2689069bd
aadhiraj <aadhiraj@users.noreply.github.com>
2020-03-27 14:46:57 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- cash.c
+++ cash.c
@@ -11,22 +11,22 @@ int main(void)
         change = get_float("Change owed: ");
     }
     while (change < 0.0);
-    int cents = round (change * 100); // rounding to count for error
+    int cents = round(change * 100);  // rounding to count for error
 
     int coins = 0;
     int tot = 0;
-    
-    coins = coins + (cents/25);
-    tot = (cents%25);
+    // long sequence of code to identify dividing, could be simplified with a loop
+    coins = coins + (cents / 25);
+    tot = (cents % 25);
         
-    coins = coins + (tot/10);
-    tot = (tot%10);
+    coins = coins + (tot / 10);
+    tot = (tot % 10);
 
-    coins = coins + (tot/5);
-    tot = (tot%5);
+    coins = coins + (tot / 5);
+    tot = (tot % 5);
 
-    coins = coins + (tot/1);
-    tot = (tot%1);
+    coins = coins + (tot / 1);
+    tot = (tot % 1);
     
     printf("%i\n", coins);
 }
