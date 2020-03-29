67b8200ee9ae42d7ceccf568551a70cc418c0932
aadhiraj <aadhiraj@users.noreply.github.com>
2020-03-29 17:11:18 +0000

    automated commit by submit50

--- substitution.c
+++ substitution.c
@@ -3,40 +3,46 @@
 #include <ctype.h>
 #include <string.h>
 
-int main (int argc, string argv[])
+int main(int argc, string argv[])
 {
     string key = argv[1];
     
+    // checking input
     if (argc == 2 && strlen(key) == 26)
     {
         string input = get_string("plaintext: ");
         printf("ciphertext: ");
-    for (int i = 0; i < strlen(input); i++)
-    {
-        if (isupper(input[i]))
+        // looping through each character
+        for (int i = 0; i < strlen(input); i++)
         {
-            int ascii = (int)input[i];
-            int keyas = ascii - 65;
-            char letter = key[keyas];
-            printf("%c", toupper(letter));
-        }
-        else if (islower(input[i]))
-        {
-            int ascii = (int)input[i];
-            int keyas = ascii - 97;
-            char letter = key[keyas];
-            printf("%c", tolower(letter));
-        }
-        else
-        {
-            printf("%c", input[i]);
+            // code for uppercase
+            if (isupper(input[i]))
+            {
+                int ascii = (int)input[i];
+                int keyas = ascii - 65;
+                char letter = key[keyas];
+                printf("%c", toupper(letter));
+            }
+            // code for lowercase
+            else if (islower(input[i]))
+            {
+                int ascii = (int)input[i];
+                int keyas = ascii - 97;
+                char letter = key[keyas];
+                printf("%c", tolower(letter));
+            }
+            // code if space, comma and all 
+            else
+            {
+                printf("%c", input[i]);
+            }
         }
     }
-    }
+    // wrong user input
     else
     {
         printf("Usage: ./substitution key\n"); 
         return (1);
     }
     printf("\n");
-    }
+}
