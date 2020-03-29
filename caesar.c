17e93c9f9d268181a2fbec2da5d0701b0e2e39c2
aadhiraj <aadhiraj@users.noreply.github.com>
2020-03-29 13:40:43 +0000

    automated commit by submit50

--- caesar.c
+++ caesar.c
@@ -12,29 +12,29 @@ int main(int argc, string argv[])
         printf("Usage: ./caesar key\n");
         return 1;
     }
-    
-    else 
+
+    else
     {
         // getting user input
         string input = get_string("plaintext: ");
         int k = atoi(argv[1]);
-        
+
         // looping through each letter and altering it using the formula
         for (int i = 0; i < strlen(input); i++)
         {
             // if uppercase
             if (input[i] >= 'A' && input[i] <= 'Z')
             {
-                input[i] = (char) ((((int)input[i] - 'A' + k) % 26) + 'A');
+                input[i] = (char)((((int)input[i] - 'A' + k) % 26) + 'A');
             }
-            
+
             // if lowercase
             if (input[i] >= 'a' && input[i] <= 'z')
             {
-                input[i] = (char) ((((int)input[i] - 'a' + k) % 26) + 'a');
+                input[i] = (char)((((int)input[i] - 'a' + k) % 26) + 'a');
             }
         }
         printf("ciphertext: %s\n", input);
-        
+
     }
 }
