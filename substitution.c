90288835942b169fc98969c2500c064f1a6028f1
aadhiraj <aadhiraj@users.noreply.github.com>
2020-03-29 16:38:35 +0000

    automated commit by check50 [check50=True]

--- substitution.c
+++ substitution.c
@@ -6,33 +6,33 @@
 int main (int argc, string argv[])
 {
     string key = argv[1];
-    
-    string input = get_string("plaintext: ");
-    
+    printf("ciphertext: ");
     if (argc != 2 || strlen(key) != 26)
     {
-        printf("Usage: ./substitution key"); 
+        printf("Usage: ./substitution key\n"); 
         return 1;
     }
     
     else
     {
+        string input = get_string("plaintext: ");
     for (int i = 0; i < strlen(input); i++)
     {
         if (isupper(input[i]))
         {
             int ascii = (int)input[i];
             int keyas = ascii - 65;
-            input[i] = toupper(key[ascii]);
+            char letter = key[keyas];
+            printf("%c", toupper(letter));
         }
         if (islower(input[i]))
         {
             int ascii = (int)input[i];
             int keyas = ascii - 97;
-            input[i] = tolower(key[keyas]);
+            char letter = key[keyas];
+            printf("%c", tolower(letter));
         }
     }
-    printf("ciphertext: %s", input);
     }
-    
+    printf("\n");
     }
