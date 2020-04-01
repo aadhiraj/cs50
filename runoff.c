ff0d09c582dd5e2145d92db36d4f95a91245348a
aadhiraj <aadhiraj@users.noreply.github.com>
2020-04-01 11:42:58 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -136,10 +136,12 @@ bool vote(int voter, int rank, string name)
             preferences[voter][rank] = i;
             return true;
         }
+        else
+        {
+            return false;
+        }
     }
     return false;
-
-    
 }
 
 // Tabulate votes for non-eliminated candidates
