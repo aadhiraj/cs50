a8f37c4e70c385ebf4c2c806a4ec482200c6fcb9
aadhiraj <aadhiraj@users.noreply.github.com>
2020-04-02 16:05:52 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

--- credit.c
+++ credit.c
@@ -10,28 +10,38 @@ int main(void)
     if (digits != 13 && digits != 15 && digits != 16)
     {
         printf("INVALID\n");
+        return 1;
     }
     int sum = 0;
+    int cc_array[digits];
+
     for (int i = 0; i < digits; i++)
     {
-        string cc_string = (string) cc_number;
-        if (i % 2 == 1)
+        cc_array[i] = cc_number % 10;
+        cc_number = cc_number / 10;
+    }
+    for (int i = 0; i < digits; i++)
+    {
+        if (cc_array[i] % 2 == 1)
         {
-            sum = sum + cc_string[i];
+            sum = sum + cc_array[i];
         }
         else
         {
-            sum = sum + 2 * cc_string[i];
+            sum = sum + 2 * cc_array[i];
         }
-        if (sum % 10 == 0)
+    }
+    if (sum % 10 == 0)
         {
             if (digits == 13)
             {
                 printf("VISA\n");
+                return 2;
             }
             else if (digits == 15)
             {
                 printf("AMEX\n");
+                return 3;
             }
             else
             {
@@ -43,6 +53,7 @@ int main(void)
                 if (first == 4)
                 {
                     printf("VISA\n");
+                    return 4;
                 }
                 else
                 {
@@ -50,6 +61,5 @@ int main(void)
                 }
             }
 
-        }
     }
 }
