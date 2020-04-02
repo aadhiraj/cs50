9857469709b63ddf3ffc0631b6b82322c2068e17
aadhiraj <aadhiraj@users.noreply.github.com>
2020-04-02 14:53:26 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -1,16 +1,18 @@
 #include <cs50.h>
 #include <stdio.h>
 
-void repeat_char (string character, int times);
+void repeat_char(string character, int times);
 
 int main(void)
 {
+    // getting user input
     int height;
     do
     {
         height = get_int("Height: ");
     }
     while (height > 8 || height < 1);
+    // looping through height
     int num = height - 1;
     for (int i = 1; i <= height; i++)
     {
@@ -19,12 +21,14 @@ int main(void)
         printf("  ");
         repeat_char("#", i);
         printf("\n");
-        num--;
+        num--; // decreasing spaces before hashes
     }
 }
 
-void repeat_char (string character, int times)
+// function
+void repeat_char(string character, int times)
 {
+    // looping through number of times repeat
     for (int i = 0; i < times; i++)
     {
         printf("%s", character);
