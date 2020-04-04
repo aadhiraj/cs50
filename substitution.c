e1a126a34ff7800fc3a2837ca63216cb1fc1c924
aadhiraj <aadhiraj@users.noreply.github.com>
2020-04-04 14:37:40 +0000

    automated commit by check50 [check50=True]

--- substitution.c
+++ substitution.c
@@ -5,58 +5,64 @@
 
 int main(int argc, string argv[])
 {
-    for (int i = 0; i < strlen(argv[1]); i++) 
-    {
-        for (int j = i + 1; j < strlen(argv[1]); j++) 
-        {
-            if (argv[1][i] == argv[1][j]) 
-            {   
-                printf("There are duplicate characters in key\n");
-                return 1;
-            }
-        }
-    }       
-    
+    // checking if no input
     if (argc != 2)
     {
         printf("Usage-- ./substitution key\n");
         return 1;
     }
 
-    else if (strlen(argv[1]) == 26)
+    // for loop to check if there are duplicates in argv[1]
+    for (int i = 0; i < strlen(argv[1]); i++)
     {
+        for (int j = i + 1; j < strlen(argv[1]); j++)
+        {
+            if (argv[1][i] == argv[1][j])
+            {
+                // returning error messages
+                printf("There are duplicate characters in key\n");
+                return 1;
+            }
+        }
+    }
+
+    // checking if lenght is equal to 26
+    if (strlen(argv[1]) == 26)
+    {
+        // looping through each character
         for (int i = 0; i < strlen(argv[1]); i++)
         {
-            if (!isalpha(argv[1][i]))
+            if (!isalpha(argv[1][i])) // not alphabet -- error
             {
                 printf("Key must contain 26 characters\n");
                 return 1;
-            
+
             }
         }
-        
-        string text = get_string("plaintext: ");
-        
+
+        string text = get_string("plaintext: "); // user input
+
+        // looping through each character again
         for (int i = 0; i < strlen(text); i++)
         {
-            if (isupper(text[i]))
+            if (isupper(text[i])) // if uppercase
             {
                 int number = (int) text[i] - 'A';
                 text[i] = toupper(argv[1][number]);
             }
-            else if (islower(text[i]))
+            else if (islower(text[i])) // if lowercase
             {
                 int numb = (int) text[i] - 'a';
                 text[i] = tolower(argv[1][numb]);
             }
         }
-        printf("ciphertext: %s\n", text);
+        printf("ciphertext: %s\n", text); // encrypted bit
         return 0;
     }
-    
+
     else
     {
-        printf("Usage-- ./substitution key\n");
+        printf("Usage-- ./substitution key\n"); // if wrong key
         return 1;
     }
 }
