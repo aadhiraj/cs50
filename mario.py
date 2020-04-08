e24230b9fd16990f2b5c349ff685c500bba90e4a
aadhiraj <aadhiraj@users.noreply.github.com>
2020-04-08 15:20:24 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -8,7 +8,7 @@ while True:
 
 # looping through height, printing spaces and hashes
 for i in range(n):
-    print(" " * (n - i -1), end = "")
-    print("#" * (i + 1), end = "")
-    print("  ", end = "")
-    print("#" * (i + 1))
+    print (" " * (n - i -1), end = "")
+    print ("#" * (i + 1), end = "")
+    print ("  ", end = "")
+    print ("#" * (i + 1))
